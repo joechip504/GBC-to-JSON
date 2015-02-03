0e146ff56168b6793d398d0b1b1445b715e5fa81
Joe Pringle <explodingp@gmail.com>
2015-02-02 22:48:43 -0500

    use map banks

--- main.py
+++ main.py
@@ -1,30 +1,56 @@
 from static.PokemonGold.mapping import town_to_hex, hex_to_FSP
-from utils import Map
+from utils import Map, Block
 import os
+import sys
 
 ROM_PATH = "static/PokemonGold/gold.gbc"
+SQUARE_SIZE = 8
 
 if __name__ == '__main__':
 
     with open(ROM_PATH, 'rb') as gold_rom:
 
-        # Let's try to create Pallet Town
-        pallet_town   = Map(name = "Bedroom", mapping = town_to_hex )
-        #pallet_town   = Map(name = "Pallet Town", mapping = town_to_hex )
-        bytes_to_read = pallet_town.end - pallet_town.start 
+        # Let's try to create your Bedroom in Pallet Town
+
+        current_map = Map(name = "Ash's Room", mapping = town_to_hex,
+                width = 4, height = 9)
+
+        bytes_to_read = current_map.end - current_map.start 
 
 
         # Skip to where pallet_town is stored on the ROM
+        ### NEED TO DOUBLE CHECK THE ADDING 1 HERE
+
         all_data = gold_rom.read()
-        relevant_hex_data = all_data[ pallet_town.start : pallet_town.end ]
+        relevant_hex_data = all_data[ current_map.start : current_map.end + 1 ]
+
+
+        #i, j = 550, 350 
+        i, j = 0, 0 
+        i_ = 0
 
         for h in relevant_hex_data:
-            offset = hex(pallet_town.end - bytes_to_read)
+            offset = hex(current_map.end - bytes_to_read)
             bytes_to_read -= 1
 
-            FSP_repr = hex_to_FSP.get(h)
+            FSP_repr = hex_to_FSP[current_map.bank].get(h)
+            b = Block(FSP_repr, i, j)
+            
+            current_map.add_block(b)
 
-            print("{}    {}   {}".format(offset, hex(h), FSP_repr))
+            i += 8 
+            i_ += 1
+            if (i_ % current_map.width == 0):
+                i_ = 0
+                i -= 8 * current_map.width
+                j += 8 
+
+
+
+        print(current_map.FSP_map())
+
+
+        
 
 
         #print(pallet_town.name, pallet_town.start, pallet_town.end)
--- scripts/parse_raw_map_dump.py
+++ scripts/parse_raw_map_dump.py
@@ -16,18 +16,28 @@ def dump_to_dict(dump):
 
     with open(dump) as f:
 
+        current_bank = None
+
         for line in f.readlines():
+
+            # Update current_bank, if necessary
+            if '=' in line:
+                current_bank = '_'.join(line.strip().strip('=').split())
+                print(current_bank) # 37_DC000
+                continue
+
+
 
             # The dump is messy and has lots of irrelevant lines we can ignore.
             line = line.strip()
-            if not(line) or '=' in line:
+            if not(line):
                 continue
 
             # Parse into    name:"Pallet Town", start:"A9497", end:"A94F0" 
             hex_locations, name = line.strip().split(' ', 1)
             start, end = hex_locations.split('-')
             
-            d[name] = (start, end)
+            d[name] = (start, end, current_bank)
 
     return d
 
